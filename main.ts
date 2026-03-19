09c1f0fb1c69ba9ece1fea6d503b0d8f35f06ee6
103432-tech <103432@isp.cz>
2026-03-19 16:02:29 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,65 @@
 hummingbird.startHummingbird()
 basic.forever(function () {
-    hummingbird.setPositionServo(FourPort.One, 90)
+    hummingbird.setPositionServo(FourPort.Four, 90)
     basic.pause(1000)
-    hummingbird.setPositionServo(FourPort.One, 180)
+    hummingbird.setPositionServo(FourPort.Four, 180)
+    basic.pause(1000)
+})
+basic.forever(function () {
+    hummingbird.setRotationServo(FourPort.Three, 90)
+    basic.pause(1000)
+    hummingbird.setRotationServo(FourPort.Three, 180)
+    basic.pause(1000)
+})
+basic.forever(function () {
+    hummingbird.setTriLED(
+    TwoPort.Two,
+    0,
+    100,
+    100
+    )
+    basic.pause(500)
+    hummingbird.setTriLED(
+    TwoPort.Two,
+    0,
+    100,
+    100
+    )
+    basic.pause(500)
+    hummingbird.setTriLED(
+    TwoPort.Two,
+    100,
+    100,
+    100
+    )
+    basic.pause(500)
+})
+basic.forever(function () {
+    hummingbird.setTriLED(
+    TwoPort.One,
+    100,
+    0,
+    0
+    )
+    basic.pause(500)
+    hummingbird.setTriLED(
+    TwoPort.One,
+    0,
+    0,
+    100
+    )
+    basic.pause(500)
+    hummingbird.setTriLED(
+    TwoPort.One,
+    0,
+    100,
+    0
+    )
+    basic.pause(500)
+})
+basic.forever(function () {
+    hummingbird.setRotationServo(FourPort.Two, 90)
+    basic.pause(1000)
+    hummingbird.setRotationServo(FourPort.Two, 180)
     basic.pause(1000)
 })
